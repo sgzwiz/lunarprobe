fa7db4a20d17b1052076462166a43cbbf5ce6ea8
sri.panyam <sri.panyam@e15cb9ce-c0c1-11dd-a933-0d089a76a3b6>
2008-12-05 00:13:58 +0000

    Removed copyright notice - included in License file.

--- trunk/client/client.py
+++ trunk/client/client.py
@@ -3,8 +3,6 @@
 """
     A simple test client for the lua debugger.
 
-    Copyright 2009 Sri Panyam
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
